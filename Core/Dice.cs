a3a3947ebf2d5bf4a8d4b04244938d5a6a39164e
Eliot <eliot.transfer@gmail.com>
2021-10-25 11:21:20 +0200

    Add Dice Class

---
+++ Core/Dice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Core
+{
+    public class Dice
+    {
+        private readonly int  _nbFaces;
+        private readonly Random _random;
+        public int Face;
+
+
+        public Dice(int nbFaces=6)
+        {
+            _nbFaces = nbFaces;
+            _random = new Random();
+            Face = _random.Next(1, _nbFaces+1);
+        }
+
+        public void Lancer()
+        {
+            Face = _random.Next(1, _nbFaces + 1);
+        }   
+    }
+}
