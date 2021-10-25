f73f1eb29bc3e18bf9619c0b6701576996753fc5
Eliot <eliot.transfer@gmail.com>
2021-10-25 17:30:20 +0200

    Add Game Class

---
+++ MinivilleConsole/Game.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+
+namespace MinivilleConsole
+{
+    public class Game : CoreGame
+    {
+        public Game(string playerName) : base(playerName)
+        {
+            
+        }
+
+        public override void Run()
+        {
+            while (true)
+            {
+                HumanTurn();
+                if (IsPlayerWin())
+                {
+                    
+                }
+                ComputerTurn();
+                if (IsComputerWin())
+                {
+                    
+                }
+            }
+            
+        }
+
+        public override void HumanTurn()
+        {
+            
+        }
+
+        public override void ComputerTurn()
+        {
+            
+        }
+    }
+}
