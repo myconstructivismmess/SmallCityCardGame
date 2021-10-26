a1e0f7683efb0e4afca7aceca43ad6627196dcbf
TNtube <51389578+TNtube@users.noreply.github.com>
2021-10-26 10:27:18 +0200

    Adding possibility to pick a specific card type

--- Core/CardStack.cs
+++ Core/CardStack.cs
@@ -1,16 +1,17 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Core {
 	public class CardStack {
 		private List<Card> _cards;
-		
+
 		private static Random _random = new Random();
 		public CardStack() {
 			// Creating a new stack of card with 6 of each cards
 			_cards = new List<Card>();
-			
+
 			for (int i = 0; i < 6; i++) {
 				if (i < 4) {
 					// Only 4 of these because of the start deck of each players
@@ -24,23 +25,23 @@ namespace Core {
 				_cards.Add(new CoffeeShop());
 				_cards.Add(new Restaurant());
 			}
-			Shuffle();
 		}
 
-		private void Shuffle() {
-			// This function use a random generator to order the list to shuffle it randomly
-			_cards = _cards.OrderBy(x => _random.Next()).ToList();
+		public int GetCardCount(CardType type) {
+			return _cards.Sum(x => x.CardType == type ? 1 : 0);
 		}
 
-		public Card PickCard() {
-			Card card = _cards.Last();
-			_cards.RemoveAt(_cards.Count - 1);
-			return card;
-		}
+		public Card? PickCard(CardType type) {
+			Card? result = null;
+			foreach (var card in _cards) {
+				if (card.CardType == type) {
+					result = card;
+					_cards.Remove(card);
+					break;
+				}
+			}
 
-		public void ReturnCard(Card card) {
-			_cards.Add(card);
-			Shuffle();
+			return result;
 		}
 	}
 }
