a2b2deaf594bd3d12ac5586b8b4566cb7963875a
Eliot <eliot.transfer@gmail.com>
2021-10-26 10:07:29 +0200

    Modified Human Turn and change Face from Dice to Value

--- MinivilleConsole/Game.cs
+++ MinivilleConsole/Game.cs
@@ -16,25 +16,34 @@ namespace MinivilleConsole
                 HumanTurn();
                 if (IsPlayerWin())
                 {
-                    
+                    // Phrase de victoire du Joueur
                 }
+
                 ComputerTurn();
                 if (IsComputerWin())
                 {
-                    
+                    // Phrase de victoire du de l'IA
                 }
             }
-            
         }
 
         public override void HumanTurn()
         {
-            
+            // Start the Human Turn
+            //Roll the Dice
+            GameDice.Roll();
+            // Display of the Dice
+            // Card Activate Blue and Green
+            HumanPlayer.PlayerTurn(GameDice.Value);
+            // Card Activate Red
+            HumanPlayer.OpponentTurn(ComputerPlayer,GameDice.Value);
+            // Display Win or Loosing money
+            // Display Ask buy a card or nothing
         }
 
         public override void ComputerTurn()
         {
-            
+            // Phrase de début de tour de l'ia
         }
     }
 }
